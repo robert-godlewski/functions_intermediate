2e7331465f7319e2a8d852c687c737ed5fa6f96a
Robert Godlewski <robtechg5@gmail.com>
2022-01-05 17:44:06 -0800

    Step 1.

--- functions_intermediate_i.py
+++ functions_intermediate_i.py
@@ -1,1 +1,24 @@
-print('hello world')
+# 1
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+print('original:', x)
+x[1][0] = 15
+print('altered:', x)
+print('original:', students)
+students[0]['last_name'] = 'Bryant'
+print('altered:', students)
+print('original:', sports_directory)
+sports_directory['soccer'][0] = 'Andres'
+print('altered:', sports_directory)
+print('original:', z)
+z[0]['y'] = 30
+print('altered:', z)
