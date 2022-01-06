4ba20d2e2e1ab9d1e11bfba8b7e63374ac758bd8
Robert Godlewski <robtechg5@gmail.com>
2022-01-05 20:03:06 -0800

    Step 2 added.

--- functions_intermediate_i.py
+++ functions_intermediate_i.py
@@ -22,3 +22,27 @@ print('altered:', sports_directory)
 print('original:', z)
 z[0]['y'] = 30
 print('altered:', z)
+
+# 2
+students = [
+    {'first_name' :  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+def iterateDictionary(li):
+    fn = 'first_name -'
+    ln = 'last_name -'
+    '''print(type(li))
+    print(li[0])
+    print(li[0]['first_name'])'''
+    i = 0
+    while i < len(li):
+        first = li[i]['first_name']
+        last = li[i]['last_name']
+        print(fn, str(first) + ',', ln, str(last))
+        i += 1
+
+iterateDictionary(students)
+
